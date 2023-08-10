6fefb9af22b538c28ce475a950bef28e279e79bd
Yashowardhan Pandey <52581482+yashowardhan992@users.noreply.github.com>
2023-08-10 20:36:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import Search from "./components/Search";
+import Forecast from "./components/Forecast/Forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = async (searchData) => {
+    const [lat, lon] = searchData.val.split(","); // Update the split format
+    const city = searchData.value; // Update the split format
+
+    const fetchWeatherData = fetch(
+      `http://localhost:8080/api/weather/details?city=${city}&unit=metric`,
+      {
+        headers: {
+          "proxy-connection": "keep-alive",
+          "proxy-host": "http://localhost:8080",
+        },
+        Origin: "http://localhost:3000",
+      }
+    );
+
+    const fetchForecast = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    try {
+      const [weatherResponse, forecastResponse] = await Promise.all([
+        fetchWeatherData,
+        fetchForecast,
+      ]);
+
+      if (weatherResponse.status === 200 && forecastResponse.status === 200) {
+        const weatherData = await weatherResponse.json();
+        const forecastData = await forecastResponse.json();
+
+        setWeatherData({ city: searchData.label, ...weatherData });
+        setForecast({ city: searchData.label, ...forecastData });
+      } else {
+        console.log("No corresponding weather or forecast data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Forecast Data:", forecast);
+  }, [forecast]); // Log forecast whenever it changes
+
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather weatherData={weatherData} />
+      {forecast && forecast.list ? <Forecast forecast={forecast} /> : <p></p>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const SPRING_API_URL = "http://localhost:8080/api/weather";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "83256463b39d35f2f297bad03ee128dc";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1c7961da88msh78ec42d56dda2fcp1a7dafjsnd892bf7b3006",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
